54da15df61c9c32354dcc1bebdcc62c9679d939d
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2022-12-12 13:22:30 +0800

    build: update homebrew formula

---
+++ scripts/update-brew-formula.sh
@@ -0,0 +1,143 @@
+#!/bin/bash
+
+#script arguments
+wheel_files=( $1 )
+homebrew_tap_repo=$2
+
+create_resources() {
+  local wheel=`realpath $1`
+  local package=$2
+  local output=$3
+
+  echo "Creating temp directory."
+  local temp=`mktemp -d`
+  pushd $temp >/dev/null
+
+  echo "Creating python virtual environment."
+  python3 -m venv venv
+  source venv/bin/activate
+
+  echo "Using poet to generate resources."
+  pip install $wheel homebrew-pypi-poet >/dev/null 2>&1
+  #also strip out resource block for referenced package
+  local resources=`poet $package 2>/dev/null | sed "/resource \"$package\" do/{N;N;N;N;d;}"`
+
+  echo "Cleanup."
+  deactivate
+  popd >/dev/null
+  rm -rf $temp
+  
+  echo "Saving resources to $output"
+  echo "$resources" > $output
+}
+
+get_metadata() {
+  local field=$1
+  grep "^$field:" $metadata | cut -f 2 -d : | xargs
+}
+
+create_formula() {
+  repo="https://github.com/${GITHUB_REPOSITORY}"
+  homepage="$repo"
+
+  wheel_file=${wheel_files[0]}
+  wheel=`basename $wheel_file`
+  echo "Creating brew formula from $wheel_file"
+
+  #determine version and release tag from .whl
+  version=None
+  version_regex="-([0-9]+\.[0-9]+\.[0-9]+)b?([0-9]*)-"
+  if [[ $wheel_file =~ $version_regex ]]; then
+    version=${BASH_REMATCH[1]}
+    version_beta=${BASH_REMATCH[2]}
+  fi
+
+  release_tag="v${version}"
+  if [[ -n $version_beta ]]; then
+    release_tag=${release_tag}-beta.${version_beta}
+  fi
+
+  echo Version: $version
+  echo Release Tag: $release_tag
+
+  url="$repo/archive/refs/tags/$release_tag.tar.gz"
+  #get other metadata from wheel
+  unzip -o $wheel_file -d . >/dev/null 2>&1
+  metadata=`echo $wheel | cut -f 1,2 -d "-"`.dist-info/METADATA
+
+  command=`get_metadata Name`
+  desc=`get_metadata Summary`
+  license=`get_metadata License`
+
+  echo "Calculating sha256 of $url..."
+  sha256=`curl -s -L $url | sha256sum | cut -f 1 -d ' '`
+
+  echo "Determining resources for $command..."
+  create_resources $wheel $command resources.txt
+  resources=`cat resources.txt`
+
+  formula=`echo ${command:0:1} | tr  '[a-z]' '[A-Z]'`${command:1}
+  ruby=${command}.rb
+  head="git+$repo.git"
+
+  echo "Outputting $ruby..."
+
+cat << EOF > $ruby
+class $formula < Formula
+  include Language::Python::Virtualenv
+
+  desc "$desc"
+  homepage "$homepage"
+  url "$url"
+  sha256 "$sha256"
+  license "$license"
+  head "$head", branch: "main"
+
+  depends_on "python@3.10"
+
+$resources
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    assert_equal "$command, version $version", shell_output(bin/"$command --version").strip
+  end
+end
+EOF
+}
+
+create_pr() {
+  export GH_TOKEN=$API_TOKEN_GITHUB
+  echo "Cloning $homebrew_tap_repo..."
+  clone_dir=`mktemp -d`
+  git clone "https://${API_TOKEN_GITHUB}@github.com/${homebrew_tap_repo}.git" $clone_dir
+
+  echo "Updating Formula/$ruby..."
+  cp $ruby $clone_dir/Formula
+
+  echo "Commiting..."
+  pushd $clone_dir
+  dest_branch="$command-update-$version"
+  git checkout -b $dest_branch
+  git add .
+  git commit --message "Updating $command to $version"
+
+  echo "Pushing $dest_branch..."
+  git push -u origin HEAD:$dest_branch
+
+  echo "Creating a pull request..."
+  gh pr create --fill
+
+  popd
+
+  echo "Cleanup."
+  rm -rf $clone_dir
+}
+
+create_formula
+create_pr
+
+echo Done.
+echo
